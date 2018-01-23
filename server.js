122158c8fea9f929a0815948f88488e66a0d236f
Aaron Gaither <gaither.a@gmail.com>
2018-01-23 01:25:52 -0800

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exphbs = require("express-handlebars");
 const routes = require('./controller/routes');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.render('home'));
 
