3ad79b5a45c8ba0a0763f0a24a0d7aa1f27383f2
Aaron Gaither <gaither.a@gmail.com>
2018-01-23 21:20:54 -0800

    template driver list

--- controller/routes.js
+++ controller/routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
+const drivers = require('../model/drivers');
 
 router.get('/', (req, res) => res.render('home'));
-
-router.get('/draft-list', (req, res) => res.render('draftList'));
+router.get('/draft-list', (req, res) => res.render('draftList', {drivers,}));
 router.get('/driver-standings', (req, res) => res.render('driverStandings'));
 router.get('/team-standings', (req, res) => res.render('teamStandings'));
 router.get('/rules', (req, res) => res.render('rules'));
