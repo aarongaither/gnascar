daf81420ae7a002193da6d881ba2d2b45fda6d78
Aaron Gaither <gaither.a@gmail.com>
2018-01-23 21:56:57 -0800

    add disqus

---
+++ model/drivers.js
@@ -0,0 +1,188 @@
+module.exports = [
+    {
+        car_no: 1, 
+        driver: "Jamie McMurray",
+        team: "Chip Ganassi Racing",
+        notable: "Has made playoffs three straight seasons."
+    },
+    {
+        car_no: 2, 
+        driver: "Brad Keselowski",
+        team: "Team Penske",
+        notable: "Reached Championship 4 for first time in ’17. AKA House Keys."
+    },
+    {
+        car_no: 3, 
+        driver: "Austin Dillon",
+        team: "Richard Childress Racing",
+        notable: "Scored first win, made playoffs for second year in a row."
+    },
+    {
+        car_no: 4, 
+        driver: "Kevin Harvick",
+        team: "Stewart-Haas Racing",
+        notable: "Reached Championship 4 for third time in four seasons. Personality like stale bread."
+    },
+    {
+        car_no: 6, 
+        driver: "Trevor Bayne",
+        team: "Roush Fenway Racing",
+        notable: "Career-best in average finish, top 10s in ’17."
+    },
+    {
+        car_no: 9, 
+        driver: "Chase Elliott",
+        team: "Hendrick Motorsports",
+        notable: "Strong end to sophomore season; first win is close. (unless Hamlin says otherwise)"
+    },
+    {
+        car_no: 10, 
+        driver: "Aric Almirola",
+        team: "Stewart-Haas Racing",
+        notable: "New team could yield big dividends for Florida native. But probably not."
+    },
+    {
+        car_no: 11, 
+        driver: "Denny Hamlin",
+        team: "Joe Gibbs Racing",
+        notable: "Led most laps since ’12; won in every full-time season. AKA Hambone."
+    },
+    {
+        car_no: 12, 
+        driver: "Ryan Blaney",
+        team: "Team Penske",
+        notable: "Driving third Penske car after breakthrough ’17 season. AKA Blarney."
+    },
+    {
+        car_no: 13, 
+        driver: "Ty Dillon",
+        team: "Germain Racing",
+        notable: "Has the luckiest number. Period. Not related to Austin, I think."
+    },
+    {
+        car_no: 14, 
+        driver: "Clint Bowyer",
+        team: "Stewart-Haas Racing",
+        notable: "Winless drought at 185 races. Nothing screams 'pick me!', like that."
+    },
+    {
+        car_no: 17, 
+        driver: "Ricky Stenhouse Jr.",
+        team: "Roush Fenway Racing",
+        notable: "The former Mr. Patrick, until Danica realized she could make more dating a quaterback."
+    },
+    {
+        car_no: 18, 
+        driver: "Kyle Busch",
+        team: "Joe Gibbs Racing",
+        notable: "Runner-up had career-best in poles, over 2,000 laps led. Entertaining jerk. AKA KY."
+    },
+    {
+        car_no: 19, 
+        driver: "Daniel Suarez",
+        team: "Joe Gibbs Racing",
+        notable: "Posted eight of 12 top 10s in second half of season. AKA Rico Suave."
+    },
+    {
+        car_no: 20, 
+        driver: "Erik Jones",
+        team: "Joe Gibbs Racing",
+        notable: "Sunoco Rookie winner steps into Matt Kenseth’s seat."
+    },
+    {
+        car_no: 21, 
+        driver: "Paul Menard",
+        team: "Wood Brothers Racing",
+        notable: "Veteran moves from one legendary race team to another."
+    },
+    {
+        car_no: 22, 
+        driver: "Joey Logano",
+        team: "Team Penske",
+        notable: "Missed postseason for first time with ‘The Captain.’ Which sounds like VD, to me."
+    },
+    {
+        car_no: 24, 
+        driver: "William Byron",
+        team: "Hendrick Motorsports",
+        notable: "Rookie’s quick climb continues, eyes Sunoco sweep."
+    },
+    {
+        car_no: 31, 
+        driver: "Ryan Newman",
+        team: "Richard Childress Racing",
+        notable: "Veteran snapped 127-race winless streak at Phoenix. So only another 100 or so to go, for another."
+    },
+    {
+        car_no: 32, 
+        driver: "Matt DiBenedetto",
+        team: "Go Fas Racing",
+        notable: "I think the team name may be ironic."
+    },
+    {
+        car_no: 34, 
+        driver: "Michael McDowell",
+        team: "Front Row Motorsports",
+        notable: "Career-best average finish in first full-time season."
+    },
+    {
+        car_no: 37, 
+        driver: "Chris Buescher",
+        team: "JTG Daugherty Racing",
+        notable: "Career-best in top 10s, average finish for new team."
+    },
+    {
+        car_no: 38, 
+        driver: "David Ragan",
+        team: "Front Row Motorsports",
+        notable: "Veteran driver just two starts from 400 in top series."
+    },
+    {
+        car_no: 41, 
+        driver: "Kurt Busch",
+        team: "Stewart-Haas Racing",
+        notable: "Daytona 500 winner has new crew chief in Billy Scott. AKA The Other Bush."
+    },
+    {
+        car_no: 42, 
+        driver: "Kyle Larson",
+        team: "Chip Ganassi Racing",
+        notable: "Four wins in ’17. Coin flip on top 10 or DNF each race."
+    },
+    {
+        car_no: 43, 
+        driver: "Darrell Wallace Jr.",
+        team: "Richard Petty Motorsports",
+        notable: "Rookie steps into ‘The King’s’ ride with Chevrolet."
+    },
+    {
+        car_no: 47, 
+        driver: "AJ Allmendinger",
+        team: "JTG Daugherty Racing",
+        notable: "Road course ace has made 145 consecutive starts. Angling for that participation trophy."
+    },
+    {
+        car_no: 48, 
+        driver: "Jimmie Johnson",
+        team: "Hendrick Motorsports",
+        notable: "‘Seven-Time’ at 83 wins, closing in on Waltrip, Allison. AKA JJ."
+    },
+    {
+        car_no: 78, 
+        driver: "Martin Truex Jr.",
+        team: "Furniture Row Racing",
+        notable: "Champ eyes repeat after eight-win, 2,253-laps-led season. AKA Terminex, Bug Killer, and BK."
+    },
+    {
+        car_no: 88, 
+        driver: "Alex Bowman",
+        team: "Hendrick Motorsports",
+        notable: "Young driver fills Dale Jr.’s seat after fill-in success in ’16."
+    },
+    {
+        car_no: 95, 
+        driver: "Kasey Kahne",
+        team: "Leavine Family Racing",
+        notable: "Got back to Victory Lane in ’17, takes over for McDowell."
+    },
+]
