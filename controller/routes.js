8c5efdb64b2d335cebdac3bcc698df465b016fcb
Aaron Gaither <gaither.a@gmail.com>
2018-02-04 22:18:57 -0800

    add pages for 2017 and 2018

--- controller/routes.js
+++ controller/routes.js
@@ -6,5 +6,9 @@ router.get('/draft-list', (req, res) => res.render('draftList', {drivers,}));
 router.get('/driver-standings', (req, res) => res.render('driverStandings'));
 router.get('/team-standings', (req, res) => res.render('teamStandings'));
 router.get('/rules', (req, res) => res.render('rules'));
+router.get('/roster-2017', (req, res) => res.render('roster-2017'));
+router.get('/roster-2018', (req, res) => res.render('roster-2018'));
+router.get('/summary-2017', (req, res) => res.render('summary-2017'));
+router.get('/summary-2018', (req, res) => res.render('summary-2018'));
 
 module.exports = router;
