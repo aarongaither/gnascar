8a16e3c5a45ec9f13088486caeda8e573e700403
Aaron Gaither <gaither.a@gmail.com>
2018-05-13 01:08:45 -0700

    add rough cletus gallery

--- controller/routes.js
+++ controller/routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const readdirSync = require('fs').readdirSync;
 const drivers = require('../model/drivers');
+const cletusPics = readdirSync('./public/cletus-pics/');
 
 router.get('/', (req, res) => res.render('home'));
 // router.get('/draft-list', (req, res) => res.render('draftList', {drivers,}));
@@ -10,4 +12,5 @@ router.get('/roster-2018', (req, res) => res.render('roster-2018'));
 router.get('/summary-2017', (req, res) => res.render('summary-2017'));
 router.get('/summary-2018', (req, res) => res.render('summary-2018'));
 router.get('/stats-2018', (req, res) => res.render('stats-2018'));
+router.get('/adventures-of-cletus', (req, res) => res.render('cletus', { cletusPics, }))
 module.exports = router;
